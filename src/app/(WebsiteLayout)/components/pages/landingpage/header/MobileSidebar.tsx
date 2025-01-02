5f224ffd63b1b385b5a4d22cda62126e88e4a5fa
Zied-bousnina <zbousnina@yahoo.com>
2025-01-02 17:38:06 +0100

    i have make mof$dification on the mobile sidebar

--- src/app/(WebsiteLayout)/components/pages/landingpage/header/MobileSidebar.tsx
+++ src/app/(WebsiteLayout)/components/pages/landingpage/header/MobileSidebar.tsx
@@ -20,45 +20,11 @@ const MobileSidebar = () => {
             <Box p={3}>
 
                 <Stack direction="column" spacing={2} >
-                    <Button color="inherit"
-                        onClick={() => setToggle(!toggle)}
-                        endIcon={<IconChevronDown width={20} />}
-                        sx={{
-                            justifyContent: 'space-between'
-                        }}>
 
-                        Demos
-                    </Button>
-                    {toggle && (
-                        <Collapse in={toggle}>
-                            <Box m="-21px">
-                                <Box ml={1}>
-                                </Box>
-                            </Box>
-                        </Collapse>
-                    )}
 
-                    <Button color="inherit"
-                        onClick={() => setToggle2(!toggle2)}
-                        endIcon={<IconChevronDown width={20} />}
-                        sx={{
-                            justifyContent: 'space-between'
-                        }}>Pages</Button>
-                    {toggle2 && (
-                        <Collapse in={toggle2}>
-                            <Box overflow="hidden" ml={1}>
-                                <AppLinks />
-                                <QuickLinks />
-                            </Box>
-                        </Collapse>
-                    )}
-                    <Button color="inherit" href="#" sx={{
-                        justifyContent: 'start'
-                    }}>Documentation</Button>
-                    <Button color="inherit" href="https://adminmart.com/support" sx={{
-                        justifyContent: 'start'
-                    }}>Support</Button>
-                    <Button color="primary" variant="contained" href="/auth/auth1/login">Login</Button>
+
+
+                    <Button color="primary" variant="contained" href="https://carvoy-7.vercel.app/">Login</Button>
                 </Stack>
             </Box>
         </>
